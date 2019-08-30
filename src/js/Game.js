458ad954921731adfec1b6f2dc24874f5c6a82a7
Gabriel Redig <glredig@gmail.com>
2019-08-30 11:08:26 -0700

    track selected squares; basic game mechanics

--- src/js/Game.js
+++ src/js/Game.js
@@ -1,5 +1,6 @@
 import FlashMessage from './FlashMessage';
 import GameBoard from './GameBoard';
+import WINNERS from './constants';
 
 export default class Game {
   constructor() {
@@ -8,14 +9,44 @@ export default class Game {
     this.build();
     this.board = new GameBoard({
       container: this.container,
+      clickHandler: this.handleSquareClick.bind(this),
+      player: 'x',
       game: this
     });
     this.currentPlayer = 'x';
+    this.playerXSelections = [];
+    this.playerOSelections = [];
   }
 
   build() {
     this.container = document.createElement('div');
     this.container.className = 'container';
     document.body.appendChild(this.container);
+  }
+
+  handleSquareClick(e) {
+    let number = parseInt(e.target.id, 10);
+
+    if (this.playerXSelections.indexOf(number) > -1 || this.playerOSelections.indexOf(number) > -1) {
+      return false;
+    }
+
+    let selection = this.currentPlayer === 'x' ? this.playerXSelections : this.playerOSelections;
+    e.target.classList.add(`selected-${this.currentPlayer}`);
+
+    selection.push(number);
+    if (this.isWinner(selection)) {
+      console.log('Winner is: ', this.currentPlayer, selection);
+    }
+    
+    this.currentPlayer = this.currentPlayer === 'x' ? 'o' : 'x';
+    this.board.updateCurrentPlayer(this.currentPlayer);
+  }
+
+  isWinner(selectionArray) {
+    return false;
+    // WINNERS.forEach((winner) => {
+    //   winner.forEach
+    // })
   }
 }
--- src/js/GameBoard.js
+++ src/js/GameBoard.js
@@ -2,18 +2,28 @@ export default class GameBoard {
   constructor(config) {
     this.game = config.game;
     this.container = config.container;
+    this.currentPlayer = config.player;
     this.build();
+    this.boardElement.addEventListener('click', config.clickHandler);
   }
 
   build() {
     this.boardElement = document.createElement('div');
     this.boardElement.className = 'board';
+    this.boardElement.classList.add(`player-${this.currentPlayer}`);
     this.container.appendChild(this.boardElement);
 
     for (let i = 0; i < 9; i++) {
       let square = document.createElement('div');
       square.className = `square square-${i}`;
+      square.id = i;
       this.boardElement.appendChild(square);
     }
+  }
+
+  updateCurrentPlayer(player) {
+    this.boardElement.classList.remove('player-x');
+    this.boardElement.classList.remove('player-o');
+    this.boardElement.classList.add(`player-${player}`);
   }
 }
